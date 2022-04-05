9414c7c225e83cf6c81335cd8988fdadc19d6309
admin <admin@admins-MacBook-Pro.local>
2022-04-05 22:49:41 +0400

    typescript adding process

--- src/store/redux/store.ts
+++ src/store/redux/store.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 
-import reducers from './reducers/reducers.js';
+import reducers from './reducers/reducers';
 
 export const store = createStore(reducers, applyMiddleware(ReduxThunk));
